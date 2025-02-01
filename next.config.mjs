2bc4ffe3b24e14b6620e75c9c6c3bc4a4a384b56
Harsh-Sinha <harsh2004sinha@gmail.com>
2025-02-01 12:05:58 +0530

    for deployments

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,9 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com','assets.aceternity.com','plus.unsplash.com'],
     },
-};
-
-module.exports = {
-    eslint: { ignoreDuringBuilds: true },
-    typescript: { ignoreBuildErrors: true }
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
